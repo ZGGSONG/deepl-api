b8f60acdd76ae0758779bb876bae1827c001b535
zggsong <zggsong@foxmail.com>
2023-01-05 09:49:28 +0800

    chore: update structure

--- core/core.go
+++ core/core.go
@@ -1,9 +1,15 @@
 package core
 
 import (
+	"api4Deeplx/global"
+	"api4Deeplx/model/deepl"
 	"api4Deeplx/router"
+	"api4Deeplx/util"
+	"encoding/json"
 	"fmt"
 	"github.com/gin-gonic/gin"
+	log "github.com/sirupsen/logrus"
+	"strconv"
 )
 
 // GinServe
@@ -11,6 +17,8 @@ import (
 //	@Description: 创建http服务
 //	@param port 监听端口
 func GinServe(port int) {
+	gin.SetMode(gin.ReleaseMode)
+
 	r := gin.Default()
 	r = router.CollectRoute(r)
 
@@ -19,4 +27,51 @@ func GinServe(port int) {
 	} else {
 		r.Run(fmt.Sprintf(":%v", port))
 	}
+}
+
+var (
+	URL         = "https://www2.deepl.com/jsonrpc"
+	Refer       = "https://www.deepl.com/"
+	ContentType = "application/json"
+	UserAgent   = "DeepL-iOS/2.4.0 iOS 15.7.1 (iPhone14,2)"
+)
+
+// Handle
+//
+//	@Description: 翻译处理函数
+//	@param sourceMsg
+func Handle(sourceMsg []string) {
+	var serveResp []string
+	text := sourceMsg[0]
+	sourceLang := sourceMsg[1]
+	targetLang := sourceMsg[2]
+	timeSpan := util.GenerateTimestamp(text)
+	id := util.CreateId()
+	sourceLang, regionalVariant := util.ConvertRegionalNameAndSourceLang(sourceLang, targetLang)
+	var reqStr = util.GenerateRequestStr(text, sourceLang, targetLang, regionalVariant, timeSpan, id)
+
+	var headers = make(map[string]string)
+	headers["Content-Type"] = ContentType
+	headers["Referer"] = Refer
+	headers["Content-Length"] = strconv.Itoa(len(reqStr))
+	headers["User-Agent"] = UserAgent
+	log.Printf("generate source: %v", reqStr)
+	body, err := util.HttpPost(URL, reqStr, headers)
+	if err != nil {
+		global.GLO_RESP_CH <- []string{err.Error(), "NOT NULL"}
+		return
+	}
+	log.Printf("translate source: %v", string(body))
+	var resp deepl.Response
+	_ = json.Unmarshal(body, &resp)
+
+	if resp.Result.Texts != nil {
+		serveResp = []string{resp.Result.Texts[0].Text, ""}
+		log.Printf("translateText: %v\n", resp.Result.Texts[0].Text)
+	} else {
+		serveResp = []string{resp.Error.Message, string(rune(resp.Error.Code))}
+		log.Printf("msg: %v\n", resp.Error.Message) //To many requests
+		log.Printf("code: %v\n", resp.Error.Code)   //1042911
+	}
+	global.GLO_RESP_CH <- serveResp
 }
--- server/server.go
+++ server/server.go
@@ -3,18 +3,6 @@ package server
 import (
 	"api4Deeplx/core"
 	"api4Deeplx/global"
-	"api4Deeplx/model/deepl"
-	"api4Deeplx/util"
-	"encoding/json"
-	log "github.com/sirupsen/logrus"
-	"strconv"
-)
-
-var (
-	URL         = "https://www2.deepl.com/jsonrpc"
-	Refer       = "https://www.deepl.com/"
-	ContentType = "application/json"
-	UserAgent   = "DeepL-iOS/2.4.0 iOS 15.7.1 (iPhone14,2)"
 )
 
 func Run() {
@@ -26,43 +14,7 @@ func Run() {
 	for {
 		select {
 		case sourceMsg := <-global.GLO_REQ_CH:
-			handle(sourceMsg)
+			go core.Handle(sourceMsg)
 		}
 	}
-}
-
-func handle(sourceMsg []string) {
-	var serveResp []string
-	text := sourceMsg[0]
-	sourceLang := sourceMsg[1]
-	targetLang := sourceMsg[2]
-	timeSpan := util.GenerateTimestamp(text)
-	id := util.CreateId()
-	sourceLang, regionalVariant := util.ConvertRegionalNameAndSourceLang(sourceLang, targetLang)
-	var reqStr = util.GenerateRequestStr(text, sourceLang, targetLang, regionalVariant, timeSpan, id)
-
-	var headers = make(map[string]string)
-	headers["Content-Type"] = ContentType
-	headers["Referer"] = Refer
-	headers["Content-Length"] = strconv.Itoa(len(reqStr))
-	headers["User-Agent"] = UserAgent
-	log.Printf("generate source: %v", reqStr)
-	body, err := util.HttpPost(URL, reqStr, headers)
-	if err != nil {
-		global.GLO_RESP_CH <- []string{err.Error(), "NOT NULL"}
-		return
-	}
-	log.Printf("translate source: %v", string(body))
-	var resp deepl.Response
-	_ = json.Unmarshal(body, &resp)
-
-	if resp.Result.Texts != nil {
-		serveResp = []string{resp.Result.Texts[0].Text, ""}
-		log.Printf("translateText: %v\n", resp.Result.Texts[0].Text)
-	} else {
-		serveResp = []string{resp.Error.Message, string(rune(resp.Error.Code))}
-		log.Printf("msg: %v\n", resp.Error.Message) //To many requests
-		log.Printf("code: %v\n", resp.Error.Code)   //1042911
-	}
-	global.GLO_RESP_CH <- serveResp
 }
