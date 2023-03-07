31a004f362ee880e6c6d60389e9b177ff21081ca
ZGGSONG <zggsong@foxmail.com>
2023-03-07 16:50:31 +0800

    feat: update

--- go.mod
+++ go.mod
@@ -2,4 +2,13 @@ module deepl_api
 
 go 1.18
 
-require github.com/goccy/go-json v0.9.7
+require (
+	github.com/fatih/color v1.14.1
+	github.com/goccy/go-json v0.9.7
+)
+
+require (
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.17 // indirect
+	golang.org/x/sys v0.3.0 // indirect
+)
--- main.go
+++ main.go
@@ -3,14 +3,29 @@ package main
 import (
 	"deepl_api/server"
 	"fmt"
+	"github.com/fatih/color"
 	"log"
 	"net/http"
 )
 
 var (
+	HOST = "127.0.0.1"
 	PORT = 8000
 	URL  = "https://www2.deepl.com/jsonrpc"
 )
+
+func init() {
+	color.Magenta("Configuration")
+	color.Blue(">> address: %v", HOST)
+	color.Blue(">> port: %v", PORT)
+	color.Magenta("Routes:")
+	color.Blue(">> (index) GET /")
+	color.Blue(">> (text_translate) POST /translate")
+	color.Magenta("Catchers:")
+	color.Blue(">> (not_found) 404")
+	color.Blue(">> (bad_request) 400")
+	color.Blue(">> (internal_err) 500")
+}
 
 func main() {
 	apiServer := server.NewAPIServer(URL, 10)
@@ -21,6 +36,6 @@ func main() {
 		w.Write([]byte("POST {\"text\": \"input your content\", \"source_lang\": \"auto\", \"target_lang\": \"ZH\"} to /translate\n\n\ngithub.com/zggsong/stranslate\n"))
 	})
 	// 启动HTTP服务器
-	fmt.Printf("deepl server starte at %v...\n", PORT)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", PORT), mux))
+	color.Magenta("Rocket has launched from http://%v:%v\n", HOST, PORT)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf("%v:%v", HOST, PORT), mux))
 }
